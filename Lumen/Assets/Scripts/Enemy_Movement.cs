7eba1a154804af5e9efb39e47ee8e208c7a12122
Tony Hendrick <thendrick@Tony-Hendricks-MacBook-Pro.local>
2017-01-21 13:10:18 -0800

    Lumen

---
+++ Lumen/Assets/Scripts/Enemy_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour {
+
+	public Transform target;
+	public float speed;
+	private float range;
+	private GameObject player_object;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
