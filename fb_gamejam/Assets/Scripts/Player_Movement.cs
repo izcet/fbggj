2315cc05a69a597adcf914e023b507e1178b8441
Tony Hendrick <thendrick@Tony-Hendricks-MacBook-Pro.local>
2017-01-21 12:17:17 -0800

    New Map

--- fb_gamejam/Assets/Scripts/Player_Movement.cs
+++ fb_gamejam/Assets/Scripts/Player_Movement.cs
@@ -35,12 +35,12 @@ public class Player_Movement : MonoBehaviour {
 			runSpeed = 1;
 		}
 
-		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition + Vector3.forward * 10f);
-		float angle = AngleBetweenPoints (transform.position, mouseWorldPosition);
-		transform.rotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
+//		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition + Vector3.forward * 10f);
+//		float angle = AngleBetweenPoints (transform.position, mouseWorldPosition);
+//		transform.rotation = Quaternion.Euler (new Vector3 (0f, 0f, angle));
 	}
 
-	float AngleBetweenPoints (Vector2 a, Vector2 b) {
-		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
-	}
+//	float AngleBetweenPoints (Vector2 a, Vector2 b) {
+//		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+//	}
 }
