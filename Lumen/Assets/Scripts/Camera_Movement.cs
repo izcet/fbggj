c3cfda59803d49850364f86d8a9216f13d972a5d
Tony Hendrick <thendrick@Tony-Hendricks-MacBook-Pro.local>
2017-01-21 14:29:50 -0800

    Boundaries

--- Lumen/Assets/Scripts/Camera_Movement.cs
+++ Lumen/Assets/Scripts/Camera_Movement.cs
@@ -26,27 +26,6 @@ public class Camera_Movement : MonoBehaviour {
 	void Update () {
 		playerView = new Vector3 (script.transform.position.x, script.transform.position.y, -10.0f);
 		mainCam.transform.position = playerView;
-		//Vector3 position = this.transform.position;
-		//print (script.transform.position.x);
-		//print (position.x);
-		//print (mainCam.transform.position.x);
-		//position = new Vector3 (script.transform.position.x, script.transform.position.y, script.transform.position.z);
-//		if (Input.GetKey ("w")) {
-//			transform.position += Vector3.up * script.speed * runSpeed * Time.deltaTime;
-//		}
-//		if (Input.GetKey("a")) {
-//			transform.position += Vector3.left * script.speed * runSpeed * Time.deltaTime;
-//		}
-//		if (Input.GetKey ("s")) {
-//			transform.position += Vector3.down * script.speed * runSpeed * Time.deltaTime;
-//		}
-//		if (Input.GetKey ("d")) {
-//			transform.position += Vector3.right * script.speed * runSpeed * Time.deltaTime;
-//		}
-//		if (Input.GetKey (KeyCode.LeftShift)) {
-//			runSpeed = script.runSpeedMultiplier;
-//		} else {
-//			runSpeed = 1;
 //		}
 	}
 }
