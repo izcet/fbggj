1a2dfc16ce43b58ace19fac0652a4068f4f7d892
Tony Hendrick <thendrick@Tony-Hendricks-MacBook-Pro.local>
2017-01-21 00:06:46 -0800

    Added camera movement

---
+++ FBGGJ/Assets/Scripts/Camera_Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour {
+
+	public GameObject otherGameObject;
+
+	private float runSpeed;
+	private Player_Movement script;
+
+	//This runs once on game start
+	void Awake ()
+	{
+		script = otherGameObject.GetComponent<Player_Movement>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = this.transform.position;
+		if (Input.GetKey ("w")) {
+			transform.position += Vector3.up * script.speed * runSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey("a")) {
+			transform.position += Vector3.left * script.speed * runSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey ("s")) {
+			transform.position += Vector3.down * script.speed * runSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey ("d")) {
+			transform.position += Vector3.right * script.speed * runSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			runSpeed = script.runSpeedMultiplier;
+		} else {
+			runSpeed = 1;
+		}
+	}
+}
--- FBGGJ/Assets/Scripts/Flashlight.cs
+++ FBGGJ/Assets/Scripts/Flashlight.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour {
 
+	public float speed;
+	public float runSpeedMultiplier;
+
+	private float runSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +16,6 @@ public class Flashlight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
