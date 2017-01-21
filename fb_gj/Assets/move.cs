7418e0744fb7ffdd26796a9a1197731a6ca0687f
seanwessmith <seanwessmith@gmail.com>
2017-01-20 19:32:56 -0800

    test

---
+++ fb_gj/Assets/move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript: MonoBehaviour
+{
+	public float movementSpeed = 10;
+
+	void Update(){
+
+		transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+
+	}
+}
